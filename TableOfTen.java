98d63910fc7f5f08c035a645499d4f6f8e7c19b7
Mohit <mohitdk2@gmail.com>
2024-07-31 13:08:17 +0530

    Batch-3 Commit

---
+++ TableOfTen.java
@@ -0,0 +1,9 @@
+public class TableOfTen {
+    public static void main(String[] args) {
+        int number = 10;
+
+        for(int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}
